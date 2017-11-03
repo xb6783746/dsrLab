c24e0609fbddb3bcc144754b14ddf3b3310bec36
vlad novikov <novikov.cs.vsu@gmail.com>
2017-11-03 22:50:00 +0300

    Добавил main.c

---
+++ linear_system/source/main.c
@@ -0,0 +1,43 @@
+#include <matrix_reader.h>
+#include <gauss.h>
+#include <expression_printer.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void printAnswer(Answer ans);
+
+int main(int argc, char** argv){
+
+    char* filename = argv[1];
+
+    Matrix matrix;
+    Answer ans;
+
+    read(filename, &matrix);
+
+    int res = solve(&matrix, &ans);
+
+    if(!res){
+
+        printf("Система имеет только тривиальное решение\n");
+
+    } else {
+
+        printAnswer(ans);
+    }
+
+    return 0;
+}
+
+void printAnswer(Answer ans){
+
+    for(size_t i = 0; i < ans.count; i++){
+
+        printf("x%d = ", i);
+
+        printExpression(ans.elems[i]);
+        
+        printf("\n");
+    }
+}
+
