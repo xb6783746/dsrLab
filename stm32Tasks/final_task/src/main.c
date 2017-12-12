cc825100a8ebdde743c01a4f1588d7e781075737
vlad novikov <novikov.cs.vsu@gmail.com>
2017-12-12 23:50:41 +0300

    Добавил main.c

--- stm32Tasks/final_task/src/main.c
+++ stm32Tasks/final_task/src/main.c
@@ -1,6 +1,23 @@
 #include <main.h>
+#include <init.h>
+#include <button_event_handler.h>
+#include <pwm_api.h>
+#include <led_api.h>
 
 int main(void) {
-
-
+    
+    initLeds();
+    initButtons();
+    initTimer();
+    initInterrupts();
+    systimerInit();
+    initLedsPWM();
+    
+    initButtonEventHandler();
+    
+    while(1){
+     
+        processButtonEvents();
+    }
+    
 }
