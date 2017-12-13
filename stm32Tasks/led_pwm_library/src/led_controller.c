f4acc13ed6de92311d1c16734b23cd63f7d983c2
vlad novikov <novikov.cs.vsu@gmail.com>
2017-12-13 23:17:27 +0300

    Задокументировал led api

--- stm32Tasks/led_pwm_library/src/led_controller.c
+++ stm32Tasks/led_pwm_library/src/led_controller.c
@@ -9,8 +9,23 @@
 #define GAMMA_CORRECTION_PARAMETER 2.2
 #endif
 
+/**
+ * @file src/led_controller.c
+ *
+ * Functions for controlling RGB led.
+ *
+ * Functions sets gamma-corected values of brightness.
+ * \f$ realValue = 255 * (\frac{x}{255})^\gamma \f$, where value of \f$ \gamma \f$ can be defined as
+ * GAMMA_CORRECTION_PARAMETER. Default value is 2.2
+ */
+
 int __errno;
 
+/**
+ * @brief Function for gamma-correcting
+ *
+ * @return \f$ 255 * (\frac{val}{255})^\gamma \f$, where \f$ \gamma \f$ is GAMMA_CORRECTION_PARAMETER
+ */
 static char gammaCorrection(char val){
     
     double tmp = val / 255.0;
