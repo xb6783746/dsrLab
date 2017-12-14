ba465683ab8ccd8b6a0c3c48f7e1d6a8ef226c4e
vlad novikov <novikov.cs.vsu@gmail.com>
2017-12-14 18:30:53 +0300

    Исправил обработчик событий кнопок

--- stm32Tasks/final_task/src/button_event_handler.c
+++ stm32Tasks/final_task/src/button_event_handler.c
@@ -1,5 +1,6 @@
 #include <button_event_handler.h>
 #include <led_api.h>
+#include <stm32f4disc_leds.h>
 
 #define CHANNEL_COUNT 3
 #define STEP 10
@@ -7,16 +8,23 @@
 extern char brightnessButtonFlag;
 extern char channelButtonFlag;
 
-static Channel order[CHANNEL_COUNT];
+static Channel extLedsChannels[CHANNEL_COUNT];
+static Stm32f4disc_leds_enum leds[CHANNEL_COUNT];
 
 static char currBrightness;
 static uint32_t currChannel;
 
 void initButtonEventHandler(){
 
-    order[0] = Red;
-    order[1] = Green;
-    order[2] = Blue;
+    initStm32DiscLeds();
+    
+    extLedsChannels[0] = Red;
+    extLedsChannels[1] = Green;
+    extLedsChannels[2] = Blue;
+    
+    leds[0] = DISC_RedLed;
+    leds[1] = DISC_GreenLed;
+    leds[2] = DISC_BlueLed;
 
     currBrightness = 255;
     currChannel = 0;
@@ -25,7 +33,8 @@ void initButtonEventHandler(){
     channelButtonFlag = 0;
     
     setRGB(0, 0, 0);
-    setBrightness(currBrightness, order[currChannel]); 
+    setBrightness(currBrightness, extLedsChannels[currChannel]); 
+    switchLed(leds[currChannel], 1);
 }
 
 void processButtonEvents(void){
@@ -36,17 +45,17 @@ void processButtonEvents(void){
         
         currBrightness = (currBrightness + STEP) % 255;
         
-        setBrightness(currBrightness, order[currChannel]);  
+        setBrightness(currBrightness, extLedsChannels[currChannel]);  
     }
 
     if(channelButtonFlag){
         
         channelButtonFlag = 0;
 
-        setBrightness(0, order[currChannel]);
-
+        switchLed(leds[currChannel], 0);
+        
         currChannel = (currChannel + 1) % CHANNEL_COUNT;
         
-        setBrightness(currBrightness, order[currChannel]);
+        switchLed(leds[currChannel], 1);
     }    
 }
