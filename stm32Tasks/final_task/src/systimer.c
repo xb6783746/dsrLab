83d01bf03233a1079c50e89701dfa7e65eb81bb2
vlad novikov <novikov.cs.vsu@gmail.com>
2017-12-21 18:58:40 +0300

    Исправил инициализацию системного таймера

--- stm32Tasks/final_task/src/systimer.c
+++ stm32Tasks/final_task/src/systimer.c
@@ -11,5 +11,8 @@ void systimerInit(void){
     
     systimer_timestamp = 0;
 
-    SysTick_Config(SystemCoreClock / SYSTIMER_FREQ);
+    //SysTick_Config(SystemCoreClock / SYSTIMER_FREQ);
+    RCC_ClocksTypeDef RCC_Clocks;
+    RCC_GetClocksFreq(&RCC_Clocks);
+    SysTick_Config(RCC_Clocks.HCLK_Frequency / SYSTIMER_FREQ);
 }
